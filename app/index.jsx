6317bd763422fa71b94284439fbb08cf47c829e5
Teamofeyy <timarnd06@gmail.com>
2024-10-06 14:56:06 +0300

    first commit

--- app/index.jsx
+++ app/index.jsx
@@ -1,12 +1,14 @@
-import { View, Text } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+import { Link } from "expo-router";
 
-export default function HomeScreen() {
+export default function App(){
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text>sdfdsfsdf</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl">IUBIP DRONE</Text>
+      <StatusBar style="auto" />
+      <Link href="/profile" style={{ color: "blue" }}>Go to profile</Link>
     </View>
-  );
+  )
 }
-
-
 
